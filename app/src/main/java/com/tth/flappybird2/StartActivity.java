f36d632a4f35493d7798865f64f06f0e711f7f49
trong <tronghuu97bl@gmail.com>
2020-11-27 19:58:02 +0700

    update dificulty

--- app/src/main/java/com/tth/flappybird2/StartActivity.java
+++ app/src/main/java/com/tth/flappybird2/StartActivity.java
@@ -1,9 +1,9 @@
 package com.tth.flappybird2;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -15,19 +15,20 @@ import android.widget.TextView;
 
 import com.tth.flappybird2.sprites.GameDificulty;
 
+import java.sql.Array;
+
 public class StartActivity extends Activity {
     private ImageView ivPlay, ivStore, ivMute;
     private TextView tvLevel;
     private SharedPreferences preferences;
+    private int dificulty_current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        preferences = getSharedPreferences("DIF",MODE_PRIVATE);
-        if(preferences!=null){
-            preferences.edit().putInt("DIF",1).apply();
-        }
+        preferences = getSharedPreferences("DIF", MODE_PRIVATE);
+        dificulty_current = preferences.getInt("DIF", 0);
         ivPlay = findViewById(R.id.imageView);
         ivPlay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,10 +49,28 @@ public class StartActivity extends Activity {
             @Override
             public void onClick(View view) {
                 //show dialog, pick Dificulty and set Dificulty
+                final AlertDialog.Builder builder = new AlertDialog.Builder(StartActivity.this);
+                builder.setTitle("Chọn mức độ khó của trò chơi");
+                final String[] item = new String[]{"Dễ", "Bình thường", "Khó"};
+                builder.setSingleChoiceItems(item, dificulty_current, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dificulty_current = i;
+                    }
+                });
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        preferences.edit().putInt("DIF", dificulty_current).apply();
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
 
-        ivMute=findViewById(R.id.imageView5);
+        ivMute = findViewById(R.id.imageView5);
         ivMute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
